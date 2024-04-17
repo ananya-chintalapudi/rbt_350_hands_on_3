90aa16c346ddf952e771ae2570bf58de9a73239a
Andriy_Malyshchak <106788506+andriymalyshchak1@users.noreply.github.com>
2024-04-17 17:45:28 -0500

    Add files via upload
    Signed-off-by: Andriy_Malyshchak <106788506+andriymalyshchak1@users.noreply.github.com>

--- reacher/inverse_kinematics.py
+++ reacher/inverse_kinematics.py
@@ -37,6 +37,7 @@ def ik_cost(end_effector_pos, guess):
     #calculated_end_effector_pos = end_effector_pos_matrix[:3, 3]
     
     # Compute the Euclidean distance (L2 norm) between the desired and calculated end-effector positions
+    
     cost = np.linalg.norm(end_effector_pos - guess)
     return cost
 
@@ -98,7 +99,7 @@ def calculate_inverse_kinematics(end_effector_pos, guess):
     previous_cost = np.inf
     # Initialize the current cost to 0.0
     cost = 0.0
-    print(guess)
+    
     for iters in range(MAX_ITERATIONS):
         # Calculate the Jacobian matrix using finite differences
 
@@ -128,5 +129,5 @@ def calculate_inverse_kinematics(end_effector_pos, guess):
         if abs(previous_cost - cost) < TOLERANCE:
             break
         previous_cost = cost
-
+    
     return guess
